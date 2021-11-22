ef2b64386b5086f857ceaeb170d77e54fa484233
karlimsc <karlianamsuarez@gmail.com>
2021-11-22 18:54:15 -0400

    Header

---
+++ beermoneybot_front/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+            <div class="header-container">
+               <a href="#plans" class="header--button">Conoce nuestros planes <span></span></a>
+            </div>
+      </header>
+      <main>
+      <section class="main-container-what">
+                <div class="main-container-what-title">
+                    <h1>Beermoney Bot</h1>
+                    <p>La próxima revolución de trading de criptomonedas, 
+                        siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
+                    obtiendo el mejor rendimiento en cada operación</p>
+                </div>
+                <div class="container-what-Img"></div>
+            </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
