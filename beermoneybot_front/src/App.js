4c2146447ee6444e36588ec4097767d96138135b
karlimsc <karlianamsuarez@gmail.com>
2021-11-22 20:30:57 -0400

    fix

--- beermoneybot_front/src/App.js
+++ beermoneybot_front/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <header>
-            <div class="header-container">
-               <a href="#plans" class="header--button">Conoce nuestros planes <span></span></a>
-            </div>
+      <header className="App-header">
+        <div className="header-container">
+        </div>
       </header>
       <main>
-      <section class="main-container-what">
-                <div class="main-container-what-title">
-                    <h1>Beermoney Bot</h1>
-                    <p>La próxima revolución de trading de criptomonedas, 
-                        siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
-                    obtiendo el mejor rendimiento en cada operación</p>
-                </div>
-                <div class="container-what-Img"></div>
-            </section>
+        <section className="main-container-what">
+        <div className="main-container-what-title">
+          <h1>Qué es</h1>
+          <h2>Beermoney Bot?</h2>
+          <p>La próxima revolución de trading de criptomonedas, 
+              siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
+          obtiendo el mejor rendimiento en cada operación</p>
+      </div>
+      <div className="container-what-Img"></div>
+        </section>
+        <section className="main-container-what">
+        <div className="main-container-what-title">
+          <h1>Cómo opera </h1>
+          <h2>al día?</h2>
+          <p>La próxima revolución de trading de criptomonedas, 
+              siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
+          obtiendo el mejor rendimiento en cada operación</p>
+      </div>
+      <div className="container-what-Img"></div>
+        </section>
+        <section className="main-container-verify">
+        <div className="main-container-verify-title">
+          <h1>Cómo verificar</h1>
+          <h2>que es real?</h2>
+          <p>La próxima revolución de trading de criptomonedas, 
+              siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
+          obtiendo el mejor rendimiento en cada operación</p>
+      </div>
+      <div className="container-what-Img"></div>
+        </section>
+        <section className="main-container-what">
+        <div className="main-container-what-title">
+          <h1>Cómo usar </h1>
+          <h2>Beermoney Bot?</h2>
+          <p>La próxima revolución de trading de criptomonedas, 
+              siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
+          obtiendo el mejor rendimiento en cada operación</p>
+      </div>
+      <div className="container-what-Img"></div>
+        </section>
+        
       </main>
     </div>
   );
