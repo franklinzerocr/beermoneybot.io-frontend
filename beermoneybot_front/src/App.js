0d80b5a181d54f1a27575efa86cc345195fe2718
karlimsc <karlianamsuarez@gmail.com>
2021-11-23 18:56:16 -0400

    table

--- beermoneybot_front/src/App.js
+++ beermoneybot_front/src/App.js
@@ -9,46 +9,89 @@ function App() {
       </header>
       <main>
         <section className="main-container-what">
-        <div className="main-container-what-title">
-          <h1>Qué es</h1>
-          <h2>Beermoney Bot?</h2>
-          <p>La próxima revolución de trading de criptomonedas, 
+          <div className="main-container-what-title">
+            <h1>Qué es</h1>
+            <h2>Beermoney Bot?</h2>
+            <p>La próxima revolución de trading de criptomonedas,
               siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
-          obtiendo el mejor rendimiento en cada operación</p>
-      </div>
-      <div className="container-what-Img"></div>
+              obtiendo el mejor rendimiento en cada operación</p>
+          </div>
+          <div className="container-what-Img"></div>
         </section>
         <section className="main-container-what">
-        <div className="main-container-what-title">
-          <h1>Cómo opera </h1>
-          <h2>al día?</h2>
-          <p>La próxima revolución de trading de criptomonedas, 
+          <div className="main-container-what-title">
+            <h1>Cómo opera </h1>
+            <h2>al día?</h2>
+            <p>La próxima revolución de trading de criptomonedas,
               siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
-          obtiendo el mejor rendimiento en cada operación</p>
-      </div>
-      <div className="container-what-Img"></div>
+              obtiendo el mejor rendimiento en cada operación</p>
+          </div>
+          <div className="container-what-Img"></div>
         </section>
         <section className="main-container-verify">
-        <div className="main-container-verify-title">
-          <h1>Cómo verificar</h1>
-          <h2>que es real?</h2>
-          <p>La próxima revolución de trading de criptomonedas, 
+          <div className="main-container-verify-title">
+            <h1>Cómo verificar</h1>
+            <h2>que es real?</h2>
+            <p>La próxima revolución de trading de criptomonedas,
               siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
-          obtiendo el mejor rendimiento en cada operación</p>
-      </div>
-      <div className="container-what-Img"></div>
+              obtiendo el mejor rendimiento en cada operación</p>
+          </div>
+          <div className="container-what-Img"></div>
         </section>
         <section className="main-container-what">
-        <div className="main-container-what-title">
-          <h1>Cómo usar </h1>
-          <h2>Beermoney Bot?</h2>
-          <p>La próxima revolución de trading de criptomonedas, 
+          <div className="main-container-what-title">
+            <h1>Cómo usar </h1>
+            <h2>Beermoney Bot?</h2>
+            <p>La próxima revolución de trading de criptomonedas,
               siendo un bot que te ayuda a navegar entre los diferentes precios y tendencias
-          obtiendo el mejor rendimiento en cada operación</p>
-      </div>
-      <div className="container-what-Img"></div>
+              obtiendo el mejor rendimiento en cada operación</p>
+          </div>
+          <div className="container-what-Img"></div>
         </section>
-        
+        <section class="main-exchange-container">
+          <div class="main-exchange-container-title">
+
+          </div>
+
+          <section class="main-table-container">
+            <div class="main-table-currency">
+              <div class="table-currency-container">
+                <table>
+                  <tr>
+                    <th>Lorem</th>
+                    <th>Lorem</th>
+                    <th>Lorem</th>
+                  </tr>
+                  <tr>
+                    <td>001</td>
+                    <td class="table_top-left">Bitcoin</td>
+                    <td class="table_top-right table_right">$1.96 </td>
+                  </tr>
+                  <tr>
+                    <td>001</td>
+                    <td>Etherium</td>
+                    <td class="table_right">$0.4 </td>
+                      
+                  </tr>
+                  <tr>
+                    <td>001</td>
+                    <td>Rippler</td>
+                    <td class="table_right">$2.5</td>
+                  </tr>
+                  <tr>
+                    <td>001</td>
+                    <td class="table_bottom-left">Stellar</td>
+                    <td class="table_bottom-right table_right">$4.9</td>
+                  </tr>
+                </table>
+                <div class="currency-date">
+                  <p>Telegram: únete </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+
       </main>
     </div>
   );
