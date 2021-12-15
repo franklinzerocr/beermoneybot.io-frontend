d9ac4f32923bf5cd8ea8fa361ebe3f6bfaa1e893
karlimsc <karlianamsuarez@gmail.com>
2021-12-15 16:25:51 -0400

    table roi almost ready

--- beermoneybot_front/src/components/TableRoi.jsx
+++ beermoneybot_front/src/components/TableRoi.jsx
@@ -4,11 +4,26 @@ import '../styles/TableRoi_style.scss'
 export const TableRoi = () => {
     return (
         <div className="grid-container-tableroi">
-            <div className="item1">Win</div>
-            <div className="item2">Loss</div>
-            <div className="item3">avg</div>  
-            <div className="item4">trade</div>
-            <div className="item5">roi</div>
+            <div className="item1">
+                <div className="tableroi-sqare">   WIN  </div>
+                <div className="tableroi-center">  20  </div>
+                </div>
+            <div className="item2">
+                <div className="tableroi-sqare">   LOSSES  </div>
+                <div className="tableroi-center">  3  </div>    
+            </div>
+            <div className="item3">
+                <div className="tableroi-sqare">   AVG  </div>
+                <div className="tableroi-center">  3%  </div>    
+            </div>  
+            <div className="item4">
+                <div className="tableroi-sqare">   TRADES  </div>
+                <div className="tableroi-center">  23  </div>    
+            </div>
+            <div className="item5">
+                <div className="tableroi-sqare">   ROI  </div>
+                <div className="tableroi-center">  10%  </div>    
+            </div>
         </div>
     )
 }
