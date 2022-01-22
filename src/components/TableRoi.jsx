e817bc01e1aa8a83bff6ade7fd01d93d56eca62a
Daniel Moran <damg75cd@gmail.com>
2022-01-22 01:09:39 -0400

    dos tablas de roi btc y usdt

--- src/components/TableRoi.jsx
+++ src/components/TableRoi.jsx
@@ -4,49 +4,68 @@ import CoinSelect from './CoinSelect';
 
 
 export const TableRoi = ({ coin, setCoin }) => {
-  const coinHandler = (coin) => {
-    let coin_ = ''
-    switch (coin) {
-      case 'USDT':
-        coin_ = 'USDT'
-        break;
-      case 'BTC':
-        coin_ = 'BTC';
-        break
-      default:
-        coin_ = 'USDT'
-        break;
-    }
-    return coin_
+  if (coin === 'USDT') {
+    return (
+      <>
+        <div className='grid-container-tableroi'>
+          <div className='item1'>
+            <div className='tableroi-sqare'> TRADES </div>
+            <div className='tableroi-center'> XX </div>
+          </div>
+          <div className='item2'>
+            <div className='tableroi-sqare'> AVG </div>
+            <div className='tableroi-center'> XX% </div>
+          </div>
+          <div className='item3'>
+            <div className='tableroi-sqare'> WIN </div>
+            <div className='tableroi-center'> XX </div>
+          </div>
+          <div className='item4'>
+            <div className='tableroi-sqare'> LOSES </div>
+            <div className='tableroi-center'> XX </div>
+          </div>
+          <div className='item5'>
+            <div className='tableroi-sqare'> ROI USDT </div>
+            <div className='tableroi-center'> XXX% </div>
+            <div id="circle"></div>
+          </div>
+        </div>
+        <div>
+          <CoinSelect coin={coin} setCoin={setCoin} />
+        </div>
+      </>
+    );
   }
-  return (
-    <>
-      <div className='grid-container-tableroi'>
-        <div className='item1'>
-          <div className='tableroi-sqare'> TRADES </div>
-          <div className='tableroi-center'> XX </div>
+  if (coin === 'BTC') {
+    return (
+      <>
+        <div className='grid-container-tableroi'>
+          <div className='item1'>
+            <div className='tableroi-sqare'> TRADES </div>
+            <div className='tableroi-center'> XX </div>
+          </div>
+          <div className='item2'>
+            <div className='tableroi-sqare'> AVG </div>
+            <div className='tableroi-center'> XX% </div>
+          </div>
+          <div className='item3'>
+            <div className='tableroi-sqare'> WIN </div>
+            <div className='tableroi-center'> XX </div>
+          </div>
+          <div className='item4'>
+            <div className='tableroi-sqare'> LOSES </div>
+            <div className='tableroi-center'> XX </div>
+          </div>
+          <div className='item5'>
+            <div className='tableroi-sqare'> ROI BTC </div>
+            <div className='tableroi-center'> XXX% </div>
+            <div id="circle"></div>
+          </div>
         </div>
-        <div className='item2'>
-          <div className='tableroi-sqare'> AVG </div>
-          <div className='tableroi-center'> XX% </div>
+        <div>
+          <CoinSelect coin={coin} setCoin={setCoin} />
         </div>
-        <div className='item3'>
-          <div className='tableroi-sqare'> WIN </div>
-          <div className='tableroi-center'> XX </div>
-        </div>
-        <div className='item4'>
-          <div className='tableroi-sqare'> LOSES </div>
-          <div className='tableroi-center'> XX </div>
-        </div>
-        <div className='item5'>
-          <div className='tableroi-sqare'> ROI {coinHandler(coin)} </div>
-          <div className='tableroi-center'> XXX% </div>
-          <div id="circle-roi"></div>
-        </div>
-      </div>
-      <div>
-        <CoinSelect coin={coin} setCoin={setCoin} />
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
