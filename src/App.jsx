404e71f569af4539cdaa9b1581579ce8a1cac973
Daniel Moran <damg75cd@gmail.com>
2022-01-27 22:40:09 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home'
 import Team from './pages/Team'
-import RoadmapCabled from './pages/RoadmapCabled'
 import Roadmap from './pages/Roadmap'
 import Thanks from './pages/Thanks'
 
@@ -15,9 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/team" element={<Team />} />
-        <Route path="/roadmap" element={<RoadmapCabled />} />
         <Route path="/thanks" element={<Thanks />} />
-        <Route path="/roadmap_test" element={<Roadmap />} />
+        <Route path="/roadmap" element={<Roadmap />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </div>
